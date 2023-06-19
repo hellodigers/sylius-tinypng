4344745e113619b55598d6ae0815e786fa9af3f2
dgnypowiczDige <75470626+dgnypowiczDige@users.noreply.github.com>
2023-06-19 10:40:27 +0200

    Add files via upload
    init

---
+++ tests/Application/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6ff7eab4599776cceeb3b1144fd6620567bee8c9',
+    'name' => 'sylius/plugin-skeleton-test-application',
+  ),
+  'versions' => 
+  array (
+    'sylius/plugin-skeleton-test-application' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6ff7eab4599776cceeb3b1144fd6620567bee8c9',
+    ),
+  ),
+);
